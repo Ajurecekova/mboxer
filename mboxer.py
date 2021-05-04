62e23f51ed3cc48a073c987b1ec61113b79c5ffd
Ajurecekova <62625071+Ajurecekova@users.noreply.github.com>
2021-05-04 15:00:00 +0200

    Update mboxer.py

--- mboxer.py
+++ mboxer.py
@@ -8,21 +8,21 @@ import hashlib
 
 def send_status(f,status_num,status_txt):
 
-    f.write(f'{status_num} {status_txt}\n'.encode('UTF-8'))
+    f.write(f'{status_num} {status_txt}\n'.encode('utf-8'))
     if status_num != '100':
-        f.write('\n'.encode('UTF-8'))
+        f.write('\n'.encode('utf-8'))
         f.flush()
 
 def write_method(f):
 
-    head1 = f.readline().decode('UTF-8')
-    head2 = f.readline().decode('UTF-8')
+    head1 = f.readline().decode('utf-8')
+    head2 = f.readline().decode('utf-8')
     a = head1.split(':')
     b = head2.split(':')
     a[1] = a[1].replace("\n", "")
     b[1] = b[1].replace("\n", "")
     n = b[1].isnumeric()
-    c = f.readline().decode('UTF-8')
+    c = f.readline().decode('utf-8')
     
     if a[0]!='Mailbox' or b[0]!= 'Content-length' or not n or '/' in a[1] or '/' in b[1]:
         send_status(f,'200','Bad request')
@@ -34,7 +34,7 @@ def write_method(f):
         
     if c=='\n' :
         send_status(f,'100','Ok')
-        f.write(('\n').encode('UTF-8'))
+        f.write(('\n').encode('utf-8'))
         m = hashlib.md5()
         line = f.read(int(b[1]))
         m.update(line)
@@ -94,11 +94,11 @@ def LS_method(f):
     send_status(f,'100','Ok')
     zoz = os.listdir(a[1])
     length = len(zoz)
-    f.write(('Number-of-messages:'+str(length)).encode('UTF-8'))
-    f.write(('\n\n').encode('UTF-8'))
+    f.write(('Number-of-messages:'+str(length)).encode('utf-8'))
+    f.write(('\n\n').encode('utf-8'))
     for i in zoz:
-        f.write(i.encode('UTF-8'))
-        f.write('\n'.encode('UTF-8'))
+        f.write(i.encode('utf-8'))
+        f.write('\n'.encode('utf-8'))
     f.flush()
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -115,7 +115,7 @@ while True:
         s.close()
         f=connected_socket.makefile('rwb')
         while True:
-            request=f.readline().decode('UTF-8')
+            request=f.readline().decode('utf-8')
             if not request:
                 # spojenie ukoncene
                 break
